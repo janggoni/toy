e366027c5ab741f4ed65874eb207d45f98748bf4
test <janggoni@uracle.co.kr>
2023-04-03 16:25:14 +0900

    깃허브 첫 테스테

---
+++ src/main/java/kr/co/prac/ToyProApplication.java
@@ -0,0 +1,13 @@
+package kr.co.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProApplication.class, args);
+	}
+
+}
